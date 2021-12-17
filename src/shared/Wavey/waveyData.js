8d1ad7af607817b3890df9051b3519e381460ce2
CUBOFIG <hfigueroa@ucol.mx>
2021-12-16 23:30:32 -0600

    [Fixxed] wavey. Fixed compatibility issues in styles with other components.

--- src/shared/Wavey/waveyData.js
+++ src/shared/Wavey/waveyData.js
@@ -2,11 +2,11 @@ export const waveyData = {
     waveyAbout: {
         classContainer: "wavey-container-right",
         classWavey: "wavey-right",
-        path:"M0.00,92.27 C304.00,8.32 216.30,192.39 500.00,109.03 L500.00,0.00 L0.00,0.00 Z"
+        path:"M0.00,49.98 C349.20,-49.98 150.00,150.00 500.00,49.98 L500.00,150.00 L0.00,150.00 Z"
     },
     waveyServices: {
         classContainer: "wavey-container-left",
         classWavey: "wavey-left",
-        path: "M0.00,92.27 C216.83,192.92 304.30,8.39 500.00,109.03 L500.00,0.00 L0.00,0.00 Z" 
+        path: "M0.00,49.98 C150.00,150.00 349.20,-49.98 500.00,49.98 L500.00,150.00 L0.00,150.00 Z" 
     }
 }
