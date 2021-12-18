915462ff249826640cbc038328d35d2cb7b28b75
CUBOFIG <hfigueroa@ucol.mx>
2021-12-18 16:31:46 -0600

    [Fixxed] sidebar and navbar navegation. navigation repair of the main components.

--- src/components/Contact/contact.jsx
+++ src/components/Contact/contact.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Contact = () => {
   return (
-    <div className="contact-us">
+    <div className="contact-us" id="contact">
       <h2 className='contact-us__title'>Don't be shy and let’s chat :D</h2>
       <h1 className="contact-us__contact">Contact Us</h1>
     </div>
