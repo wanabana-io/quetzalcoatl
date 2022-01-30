28db6ccac8aa0352088547e4980a8aa9586dac7a
Juanequex <ramon.g.uz37@gmail.com>
2022-01-29 22:14:55 -0600

    Add services image into firebase
    We Added the services images on storage firebase to show its on our services section.

--- src/components/Services/Card.jsx
+++ src/components/Services/Card.jsx
@@ -1,8 +1,13 @@
+import useFirabeImage from '../../hooks/useFirebaseImage';
+
 const Card = ({img, icon, title, subtitle, description}) => {
+  
+  const {url} = useFirabeImage(img);
+  
   return (
     <>
       <div className="card-container">
-        <img src={img} alt="Planning" className="card-image" />
+        { url && <img src={url} alt="Planning" className="card-image" />}
         <div className="information-container">
           <div className="title-container">
             <img src={icon} alt="Planning icon" className="title-container__icon"/>
