2407be7af4be69a83269e5c22d78a6ae3e96d9fd
CUBOFIG <hfigueroa@ucol.mx>
2022-02-04 19:48:34 -0600

    fix-waveys

--- src/shared/Wavey/wavey.jsx
+++ src/shared/Wavey/wavey.jsx
@@ -1,30 +1,30 @@
 import React from "react";
-import { waveyData } from './waveyData';
+import { waveyData } from "./waveyData";
 
 const Wavey = ({ reverseWavey }) => {
-	const data = reverseWavey ? waveyData.waveyAbout : waveyData.waveyServices
+  const data = reverseWavey ? waveyData.waveyAbout : waveyData.waveyServices;
 
-	const { path, classContainer, classWavey } = data
+  const { path, classContainer, classWavey } = data;
 
-
-	return(
-		<div className="wavey">
-			<div className="wavey__relative">
-			<div className={`wavey-container ${classContainer}`}>
-				<svg viewBox="0 0 500 150" preserveAspectRatio="none" className="wavey-sizes">
-					<path d={path}
-						className={ classWavey }
-					>
-					</path>
-				</svg>
-			</div>
-			</div>
-		</div>
-	)
-}
-
-Wavey.defaultProps = {
-  reverseWavey:false
+  return (
+    <div className="wavey">
+      <div className="wavey__relative">
+        <div className={`wavey-container ${classContainer}`}>
+          <svg
+            viewBox="0 0 500 150"
+            preserveAspectRatio="none"
+            className="wavey-sizes"
+          >
+            <path d={path} className={classWavey}></path>
+          </svg>
+        </div>
+      </div>
+    </div>
+  );
 };
 
-export default Wavey
+Wavey.defaultProps = {
+  reverseWavey: false,
+};
+
+export default Wavey;
